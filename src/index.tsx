7230d76b6e0fc25bc3aae2fce1a9be36f3127c62
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-08-25 17:06:13 -0300

    feat: add fontawesome

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from "./App";
 
 render(() => <App />, document.getElementById("root") as HTMLElement);
