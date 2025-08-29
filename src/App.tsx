e66d16073ee53b5c2bff247567baab474d97884f
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-08-29 17:20:19 -0300

    style: prepare dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import RepositoryPage from "./pages/RepositoryPage";
 import "./index.css";
 
 export default function App() {
+
+  if (localStorage.theme === "dark") {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+}
+
   return (
     <div class="h-screen w-screen flex flex-col">
       <RepositoryPage />
