bfb4301cea061ae07af567b821dda55e5b1b3f4b
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-08-30 10:48:50 -0300

    feat: add button close on repository tab

--- src/components/repo/TabBar.tsx
+++ src/components/repo/TabBar.tsx
@@ -7,20 +7,30 @@ export default function TabBar(props: {
   repos: Repo[];
   active: string | null;
   onChangeActive: (id: string) => void;
+  onClose: (id: string) => void; 
 }) {
   return (
     <div class="flex border-b bg-gray-200 dark:bg-gray-900 dark:border-gray-900">
       {props.repos.map(repo => (
-        <button
-          class={`px-4 py-2 ${
+        <div
+          class={`px-4 py-2 cursor-pointer ${
             props.active === repo.path
               ? "bg-white border-t border-l border-r -mb-px dark:bg-gray-800 dark:border-gray-900"
               : "bg-gray-200 hover:bg-gray-300 dark:bg-gray-900 dark:hover:bg-gray-800"
           }`}
           onClick={() => props.onChangeActive(repo.path)}
         >
-          {repo.name}
-        </button>
+          <span>{repo.name}</span>
+          <button
+            class="ml-2 text-gray-500 hover:text-red-500"
+            onClick={e => {
+              e.stopPropagation();
+              props.onClose(repo.path);
+            }}
+          >
+            <i class="fa fa-times"></i>
+          </button>
+        </div>
       ))}
     </div>
   );
