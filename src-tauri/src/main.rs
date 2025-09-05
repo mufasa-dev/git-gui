06a482cb885005ee83eaebcecfdda0f7e920a601
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-09-05 16:37:24 -0300

    feat: add tauri storage plugin

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,7 @@ mod commands;
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_store::Builder::default().build())
         .invoke_handler(tauri::generate_handler![
             commands::repo::open_repo,
             commands::branch::list_branches,
