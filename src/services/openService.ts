eecd67e86a2beaae33a4e556a56545c0910d7f78
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-09-05 23:20:18 -0300

    feat: create open service

---
+++ src/services/openService.ts
@@ -0,0 +1,21 @@
+import { invoke } from "@tauri-apps/api/core";
+
+export async function openConsole(path: string): Promise<string> {
+  return await invoke("open_console", { path });
+}
+
+export async function openFileManager(path: string): Promise<string[]> {
+  return await invoke("open_file_manager", { path });
+}
+
+export async function openBrowser(path: string): Promise<string[]> {
+  return await invoke("open_browser", { path });
+}
+
+export async function openVsCode(path: string): Promise<string[]> {
+  return await invoke("open_vscode", { path });
+}
+
+export async function openBash(path: string): Promise<string[]> {
+  return await invoke("open_git_bash", { path });
+}
