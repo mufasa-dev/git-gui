5095b1d3c86f6fda12423941c85d8e5523861eeb
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-09-07 21:49:34 -0300

    feat: create context menu component

---
+++ src/components/ui/ContextMenu.tsx
@@ -0,0 +1,39 @@
+import { For, JSX } from "solid-js";
+
+export type ContextMenuItem = {
+  label: string;
+  action: () => void;
+};
+
+type ContextMenuProps = {
+  items: ContextMenuItem[];
+  position: { x: number; y: number };
+  onClose: () => void;
+};
+
+export default function ContextMenu(props: ContextMenuProps) {
+  return (
+    <div
+      class="absolute bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded shadow-md z-50"
+      style={{ top: `${props.position.y}px`, left: `${props.position.x}px` }}
+    >
+      <ul class="py-1 text-sm">
+        <For each={props.items}>
+          {(item) => (
+            <li>
+              <button
+                class="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600"
+                onClick={() => {
+                  item.action();
+                  props.onClose();
+                }}
+              >
+                {item.label}
+              </button>
+            </li>
+          )}
+        </For>
+      </ul>
+    </div>
+  );
+}
