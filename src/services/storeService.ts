4fd889f7fbb787000bb156d44dfe502c1068f9a7
Bruno Ribeiro <bruno.ribeiro@e-construmarket.com.br>
2025-09-19 14:53:10 -0300

    feat: ajust store service

--- src/services/storeService.ts
+++ src/services/storeService.ts
@@ -1,17 +1,23 @@
 import { Store } from "@tauri-apps/plugin-store";
 import type { Repo } from "../models/Repo.model";
 
-const store = await Store.load(".settings.dat");
-
 const KEY = "repos";
 
+let store: Store;
+
+async function initStore() {
+  store = await Store.load(".settings.dat");
+}
+
 export async function saveRepos(repos: Repo[]) {
+  if (!store) await initStore();
   const uniquePaths = [...new Set(repos.map(r => r.path))];
   await store.set(KEY, uniquePaths);
   await store.save();
 }
 
 export async function loadRepos(): Promise<string[]> {
+  if (!store) await initStore();
   const repos = await store.get<string[]>(KEY);
   return repos ?? [];
 }
