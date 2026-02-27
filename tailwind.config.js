1ad89c0a4fa286e9d804f45e8b75f2ed8462dd63
Bruno <bruno.ribeiro96@hotmail.com.br>
2026-02-26 21:22:50 -0300

    style: add animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'bounce-in': {
+          '0%': { transform: 'scale(0.95) translateX(10px)', opacity: '0' },
+          '50%': { transform: 'scale(1.02) translateX(-2px)' },
+          '100%': { transform: 'scale(1) translateX(0)', opacity: '1' },
+        },
+        'fade-out': {
+          '0%': { transform: 'scale(1)', opacity: '1' },
+          '100%': { transform: 'scale(0.95)', opacity: '0' },
+        }
+      },
+      animation: {
+        'bounce-in': 'bounce-in 0.35s ease-out forwards',
+        'fade-out': 'fade-out 0.2s ease-in forwards',
+      },
+    },
     fontSize: {
       base: "14px",
     },
