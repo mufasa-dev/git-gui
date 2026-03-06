6843372dfa9ce54c868e06e4079505b952874c59
Bruno <bruno.ribeiro96@hotmail.com.br>
2026-03-05 22:30:03 -0300

    feat: create fn to checkout to remote branch

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -14,6 +14,7 @@ fn main() {
             commands::branch::get_current_branch,
             commands::branch::checkout_branch,
             commands::branch::create_branch,
+            commands::branch::checkout_remote_branch,
             commands::branch::delete_branch,
             commands::branch::delete_remote_branch,
             commands::commit::list_commits,
