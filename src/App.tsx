ffdb9369f8c500486161e2c0651eaeb512747667
Bruno Ribeiro <bruno.ribeiro96@hotmail.com.br>
2026-04-20 22:35:04 -0300

    feat: ajust initial page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
+import { createSignal, Show, onMount } from "solid-js";
 import RepositoryPage from "./pages/RepositoryPage";
 import "./index.css";
 import { Toaster } from "solid-toast";
 import { LoadingProvider } from "./components/ui/LoadingContext";
+import LoginPage from "./pages/Login";
 
 export default function App() {
+  const [token, setToken] = createSignal<string | null>(null);
 
-  if (localStorage.theme === "dark") {
+  onMount(() => {
+    const savedToken = localStorage.getItem("rivers_token");
+    if (savedToken) setToken(savedToken);
+
+    // Lógica do Dark Mode (Manter o fundo bem escuro)
+    localStorage.theme = "dark"; // Forçar dark mode para o estilo da imagem
     document.documentElement.classList.add("dark");
-  } else {
-    document.documentElement.classList.remove("dark");
-}
+  });
 
   return (
     <div class="h-screen w-screen flex flex-col">
       <Toaster position="bottom-right" gutter={8} />
+      
       <LoadingProvider>
-        <RepositoryPage />
+        {/* Só mostra a aplicação principal se houver token */}
+        <Show 
+          when={token()} 
+          fallback={<LoginPage onLoginSuccess={(t: any) => setToken(t)} />}
+        >
+          <RepositoryPage />
+        </Show>
       </LoadingProvider>
     </div>
   );
